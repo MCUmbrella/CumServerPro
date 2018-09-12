1138b7ccf7f51ff707da708a6eba97ffc8235221
Luohuayu <i@qqq.moe>
2018-09-12 17:15:56 +0800

    init

---
+++ src/main/java/luohuayu/CatServer/CatServer.java
@@ -0,0 +1,14 @@
+package luohuayu.CatServer;
+
+public class CatServer {
+	private static final String version = "1.0.0";
+	private static final String native_verson = "v1_12_R1";
+
+	public static String getVersion(){
+		return version;
+	}
+
+    public static String getNativeVersion() {
+        return native_verson;
+    }
+}
