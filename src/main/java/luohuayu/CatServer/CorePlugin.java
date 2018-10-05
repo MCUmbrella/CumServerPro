3cda9829a8be779474c2cb8b89e887236f8930f4
Luohuayu <i@qqq.moe>
2018-10-05 22:48:35 +0800

    兼容Bukkit插件发包

---
+++ src/main/java/luohuayu/CatServer/CorePlugin.java
@@ -0,0 +1,38 @@
+package luohuayu.CatServer;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import luohuayu.CatServer.remapper.NetworkTransformer;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class CorePlugin implements IFMLLoadingPlugin {
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[] {
+                NetworkTransformer.class.getCanonicalName()
+        };
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
