bfc52e2eef9c91090f429130302a708765e1960c
Luohuayu <i@qqq.moe>
2018-10-16 13:00:01 +0800

    移除重复的ClassLoader remap

--- src/main/java/luohuayu/CatServer/remapper/ReflectionMethods.java
+++ src/main/java/luohuayu/CatServer/remapper/ReflectionMethods.java
@@ -57,13 +57,4 @@ public class ReflectionMethods {
         }
         return pMethod.getName();
     }
-
-    public static Class<?> getClass(String pClazzName) throws ClassNotFoundException {
-        return getClass((ClassLoader)null, pClazzName);
-    }
-    
-    public static Class<?> getClass(ClassLoader pLoader, String pClazzName) throws ClassNotFoundException {
-        String tMappedClass = ReflectionTransformer.jarMapping.mapClass(pClazzName.replace('.', '/')).replace('/', '.');
-        return pLoader == null ? Class.forName(tMappedClass) : pLoader.loadClass(tMappedClass);
-    }
 }
