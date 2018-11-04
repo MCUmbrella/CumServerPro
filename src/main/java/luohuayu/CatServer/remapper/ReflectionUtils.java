135dc52070ac485e505225ebbd46351480b8cf74
Luohuayu <i@qqq.moe>
2018-11-05 02:16:54 +0800

    remap反射工具类不再使用过时方法

--- src/main/java/luohuayu/CatServer/remapper/ReflectionUtils.java
+++ src/main/java/luohuayu/CatServer/remapper/ReflectionUtils.java
@@ -1,46 +1,15 @@
 package luohuayu.CatServer.remapper;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-/**
- * Based on Apache's ReflectionUtil
- * @author Maxqia
- */
 public class ReflectionUtils {
 
-    private static final Method GET_CALLER_CLASS;
-    private static final int JDK_7u25_OFFSET;
-
-    static {
-        try {
-            int jdkOffset = 0;
-            final Class<?> reflection = Class.forName("sun.reflect.Reflection");
-            Method getCallerClass = reflection.getMethod("getCallerClass", int.class);
-
-            Object test = getCallerClass.invoke(null, 1);
-            if (test.equals(reflection)) {
-                System.out.println("WARNING : You are using Java 1.7.0_25 which has a broken implementation of Reflection.getCallerClass.");
-                System.out.println("WARNING : You should upgrade to at least Java 1.7.0_40 or later.");
-                jdkOffset = 1;
-            }
-
-            GET_CALLER_CLASS = getCallerClass;
-            JDK_7u25_OFFSET = jdkOffset;
-        } catch (ReflectiveOperationException e) {
-            throw new RuntimeException("getCallerClass doesn't exist!");
-        }
-    }
-
     public static Class<?> getCallerClass(int skip) {
-        if (GET_CALLER_CLASS != null) {
-            try {
-                return (Class<?>) GET_CALLER_CLASS.invoke(null, skip + 1 + JDK_7u25_OFFSET);
-            } catch (ReflectiveOperationException e) {
-                throw new AssertionError(e.getMessage());
+        final Class<?>[] clazz = new Class<?>[1];
+        new SecurityManager() {
+            {
+                clazz[0] = getClassContext()[skip + 1];
             }
-        }
-        throw new RuntimeException("getCallerClass doesn't exist!");
+        };
+        return clazz[0];
     }
 
     public static ClassLoader getCallerClassloader() {
