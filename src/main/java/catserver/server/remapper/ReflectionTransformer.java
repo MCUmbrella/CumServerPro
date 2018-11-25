16a187e870e56090f96c6e78e2349e6e60830ee5
Luohuayu <i@qqq.moe>
2018-11-25 22:20:27 +0800

    Revert "将URLClassLoader remap到CatURLClassLoader"
    This reverts commit 43f244d7cec5eda0f380c3179f0f8460e04f5617.

--- src/main/java/catserver/server/remapper/ReflectionTransformer.java
+++ src/main/java/catserver/server/remapper/ReflectionTransformer.java
@@ -20,7 +20,6 @@ import org.objectweb.asm.tree.MethodNode;
 
 import net.md_5.specialsource.JarMapping;
 import net.md_5.specialsource.provider.JointProvider;
-import org.objectweb.asm.tree.TypeInsnNode;
 
 public class ReflectionTransformer {
     public static final String DESC_ReflectionMethods = Type.getInternalName(ReflectionMethods.class);
@@ -61,25 +60,11 @@ public class ReflectionTransformer {
         ClassReader reader = new ClassReader(code); // Turn from bytes into visitor
         ClassNode node = new ClassNode();
         reader.accept(node, 0); // Visit using ClassNode
-        boolean remapCL = false;
-        if (node.superName.equals("java/net/URLClassLoader")) {
-            node.superName = "catserver/server/remapper/CatURLClassLoader";
-            remapCL = true;
-        }
 
         for (MethodNode method : node.methods) { // Taken from SpecialSource
             ListIterator<AbstractInsnNode> insnIterator = method.instructions.iterator();
             while (insnIterator.hasNext()) {
                 AbstractInsnNode next = insnIterator.next();
-
-                if (next instanceof TypeInsnNode) {
-                    TypeInsnNode insn = (TypeInsnNode) next;
-                    if (insn.getOpcode() == Opcodes.NEW && insn.desc.equals("java/net/URLClassLoader")) { // remap new URLClassLoader
-                        insn.desc = "catserver/server/remapper/CatURLClassLoader";
-                        remapCL = true;
-                    }
-                }
-
                 if (!(next instanceof MethodInsnNode)) continue;
                 MethodInsnNode insn = (MethodInsnNode) next;
                 switch (insn.getOpcode()) {
@@ -88,9 +73,6 @@ public class ReflectionTransformer {
                         break;
                     case Opcodes.INVOKESTATIC:
                         remapForName(insn);
-                        break;
-                    case Opcodes.INVOKESPECIAL:
-                        if (remapCL) remapURLClassLoader(insn);
                         break;
                 }
 
@@ -142,10 +124,5 @@ public class ReflectionTransformer {
         method.setOpcode(Opcodes.INVOKESTATIC);
         method.owner = DESC_ReflectionMethods;
         method.desc = Type.getMethodDescriptor(returnType, args.toArray(new Type[args.size()]));
-    }
-
-    public static void remapURLClassLoader(MethodInsnNode method) {
-        if (!(method.owner.equals("java/net/URLClassLoader") && method.name.equals("<init>"))) return;
-        method.owner = "catserver/server/remapper/CatURLClassLoader";
     }
 }
