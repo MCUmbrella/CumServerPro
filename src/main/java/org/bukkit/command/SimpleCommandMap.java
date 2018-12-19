088536adc35352432bef5dd0eb32e53d93fede50
Luohuayu <i@qqq.moe>
2018-12-19 23:49:41 +0800

    增加运行时加载/卸载插件指令

--- src/main/java/org/bukkit/command/SimpleCommandMap.java
+++ src/main/java/org/bukkit/command/SimpleCommandMap.java
@@ -29,9 +29,10 @@ public class SimpleCommandMap implements CommandMap {
 
     private void setDefaultCommands() {
         register("bukkit", new VersionCommand("version"));
-        register("bukkit", new ReloadCommand("reload"));
+        // register("bukkit", new ReloadCommand("reload"));
         register("bukkit", new PluginsCommand("plugins"));
         register("bukkit", new TimingsCommand("timings"));
+        register("catserver", new catserver.server.command.CommandPlugin("plugin"));
     }
 
     public void setFallbackCommands() {
