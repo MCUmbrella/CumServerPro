aeb3a77203ce9d94b29bc94a0715d509c6455229
KomiMoe <i@komi.moe>
2019-01-02 22:42:54 +0800

    编译时去除SideOnly

--- buildSrc/src/main/java/gradle/task/ShrinkJarTask.java
+++ buildSrc/src/main/java/gradle/task/ShrinkJarTask.java
@@ -46,29 +46,32 @@ public class ShrinkJarTask extends DefaultTask {
         ClassNode classNode = new ClassNode();
         ClassReader classReader = new ClassReader(input);
         classReader.accept(classNode, 0);
+        if (classNode.visibleAnnotations != null)
+        classNode.visibleAnnotations.removeIf(annotationNode -> isClientAnnotation(annotationNode));
 
-        if (containClientAnnotation(classNode.visibleAnnotations))
-            return null;
-
-        classNode.fields.removeIf(field -> containClientAnnotation(field.visibleAnnotations));
-        classNode.methods.removeIf(method -> containClientAnnotation(method.visibleAnnotations));
+        classNode.fields.forEach(field -> {
+            if (field.visibleAnnotations != null)
+            field.visibleAnnotations.removeIf(this::isClientAnnotation);
+        });
+        classNode.methods.forEach(method -> {
+            if (method.visibleAnnotations != null)
+            method.visibleAnnotations.removeIf(this::isClientAnnotation);
+        });
 
         ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
         classNode.accept(writer);
         return writer.toByteArray();
     }
 
-    private boolean containClientAnnotation(List<AnnotationNode> annotations) {
-        if (annotations == null)
+    private boolean isClientAnnotation(AnnotationNode annotation) {
+        if (annotation == null)
             return false;
-        for (AnnotationNode annotation : annotations) {
-            if (annotation.desc.equals("Lnet/minecraftforge/fml/relauncher/SideOnly;") && annotation.values != null) {
-                for (int x = 0; x < annotation.values.size() - 1; x += 2) {
-                    Object key = annotation.values.get(x);
-                    Object value = annotation.values.get(x + 1);
-                    if (key.equals("value") && value instanceof String[] && ((String[]) value)[1].equals("CLIENT"))
-                        return true;
-                }
+        if (annotation.desc.equals("Lnet/minecraftforge/fml/relauncher/SideOnly;") && annotation.values != null) {
+            for (int x = 0; x < annotation.values.size() - 1; x += 2) {
+                Object key = annotation.values.get(x);
+                Object value = annotation.values.get(x + 1);
+                if (key.equals("value") && value instanceof String[] && ((String[]) value)[1].equals("CLIENT"))
+                    return true;
             }
         }
         return false;
