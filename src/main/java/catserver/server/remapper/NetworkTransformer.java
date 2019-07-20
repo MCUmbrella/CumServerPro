9a567f2622c8eb1b9e60d77675d1f2e01d0fb9f6
Luohuayu <i@qqq.moe>
2019-07-20 14:21:02 +0800

    Revert "修复修复反射发包"
    This reverts commit f7073d3d

--- src/main/java/catserver/server/remapper/NetworkTransformer.java
+++ src/main/java/catserver/server/remapper/NetworkTransformer.java
@@ -44,7 +44,23 @@ public class NetworkTransformer implements IClassTransformer {
         ClassNode classNode = new ClassNode();
         new ClassReader(basicClass).accept(classNode, 0);
         ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+
+        /*
+         * public void sendPacket(Packet<?> packetIn) {
+         *      super.func_147359_a(packetIn);
+         * }
+         */
+
+        MethodVisitor mv = classWriter.visitMethod(ACC_PUBLIC, "sendPacket", "(Lnet/minecraft/network/Packet;)V", "(Lnet/minecraft/network/Packet<*>;)V", null);
+        mv.visitCode();
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitVarInsn(ALOAD, 1);
+        mv.visitMethodInsn(INVOKESPECIAL, "net/minecraft/network/NetHandlerPlayServer", "func_147359_a", "(Lnet/minecraft/network/Packet;)V", true);
+        mv.visitInsn(RETURN);
+        mv.visitEnd();
+
         classNode.access = ACC_SUPER + ACC_PUBLIC;
+
         classNode.accept(classWriter);
         return classWriter.toByteArray();
     }
